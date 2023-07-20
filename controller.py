08db159f0cc56d7dc59294053d7cfab0697639e0
Soubhik Gon <119437069+mogulcoder26@users.noreply.github.com>
2023-07-20 21:04:43 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ starting_coords = [(0, 0), (-20, 0), (-40, 0)]
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('My Snake Game')
+screen.title('Python Pursuit')
 screen.tracer(0)
 segment_array = []
 
